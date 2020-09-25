97e783cec56fdd8336ef8dcad0201505e8bb8c5d
Salem Eid <salem.eid@gmail.com>
2020-09-25 14:46:24 +0300

    Updates

---
+++ precache-manifest.0707763d44caa21343c67254369557bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bf2c8650c3902e0b4562f70677b078",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f40ceaf074872e5af0cb",
+    "url": "/monster-rolodex/static/css/main.f7d3a3c1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40ceaf074872e5af0cb",
+    "url": "/monster-rolodex/static/js/main.97d090fb.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
